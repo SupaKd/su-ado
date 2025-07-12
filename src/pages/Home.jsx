494c0e8d72d9f56ccc2f5b0a55dc4ed6e9a1102d
supaKd <kevin.khek@gmail.com>
2025-07-13 00:17:08 +0200

    footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,36 @@ import Method from "./Acceuil/Method";
 import Question from "./Acceuil/Question";
 import LogoSlider from "../components/LogoSlider";
 import Img from "../pages/Acceuil/Img";
+import ScrollRevealWrapper from "../components/ScrollRevealWrapper";
+
 function Home() {
   return (
     <main>
-      <Hero />
-      <Service />
-      <Method />
-      <LogoSlider />
-      <Img />
-      <Question />
+      <ScrollRevealWrapper>
+        <Hero />
+      </ScrollRevealWrapper>
+
+      <ScrollRevealWrapper delay={0.1}>
+        <Service />
+      </ScrollRevealWrapper>
+
+      <ScrollRevealWrapper delay={0.2}>
+        <Method />
+      </ScrollRevealWrapper>
+
+      <ScrollRevealWrapper delay={0.3}>
+        <LogoSlider />
+      </ScrollRevealWrapper>
+
+      <ScrollRevealWrapper delay={0.4}>
+        <Img />
+      </ScrollRevealWrapper>
+
+      <ScrollRevealWrapper delay={0.5}>
+        <Question />
+      </ScrollRevealWrapper>
     </main>
   );
 }
+
 export default Home;
